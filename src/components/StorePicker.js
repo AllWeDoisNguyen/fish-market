fa68ea7f25dc7cb3f450d2939e4b3597951c315d
Christina Nguyen <cnguyen2@sapient.com>
2018-07-30 16:34:07 -0400

    binding this where this is not implicit so we can console.log in goToStore function

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,22 +2,29 @@ import React from 'react';
 import {getFunName} from '../helpers';
 
 class StorePicker extends React.Component {
+
+    // constructor() {
+    //     super();
+    //     this.goToStore = this.goToStore.bind(this);
+    // }
+
     goToStore(event) {
         event.preventDefault();
         console.log('Url has been changed');
         // Es6 doesn't need a comma after this
         // First grab text from input box
+        console.log(this);
         // second, transition from / to /store/:storeId
 
     }
 
     render() {
         return (
-            <form className="store-selector" onSubmit={this.goToStore}>
+            <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
             {/* Dont put comments at top level!*/}
             {/* render is tied to the class and so "this" referrs to StorePicker class*/}
                 <h2>Please Enter A Store</h2>
-                <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+                <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}} />
                 <button type="submit">Visit Store -></button>
             </form>
         )
