dd450cc63d1005aca84731a7e54f5db1edf4d33a
Christina Nguyen <cnguyen2@sapient.com>
2018-07-30 15:22:32 -0400

    successfuly console.log with onSubmit function to change url of storePicker class

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,10 +2,19 @@ import React from 'react';
 import {getFunName} from '../helpers';
 
 class StorePicker extends React.Component {
+    goToStore() {
+        console.log('Url has been changed');
+        // Es6 doesn't need a comma after this
+        // First grab text from input box
+        // second, transition from / to /store/:storeId
+
+    }
+
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
             {/* Dont put comments at top level!*/}
+            {/* render is tied to the class and so "this" referrs to StorePicker class*/}
                 <h2>Please Enter A Store</h2>
                 <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
                 <button type="submit">Visit Store -></button>
