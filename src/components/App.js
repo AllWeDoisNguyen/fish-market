1986375b79ce4d821fa9c622fb963c082b66687e
Christina Nguyen <cnguyen2@sapient.com>
2018-08-15 13:35:58 -0400

    passing addFish function to inventory down to children via props, add addFish function to addFishForm

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,30 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+
+    constructor() {
+        super();// needed to use `this`
+
+        this.addFish = this.addFish.bind(this);
+        // getinitialState
+        this.state = {
+            fishes: {},
+            order: {}
+        };
+    }
+
+addFish(fish) {
+    // update state
+    const fishes = {...this.state.fishes};
+    // add in our new fish
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    // this.state.fishes.fish1 = fish;
+    // set state
+    // this.setState({fishes: fishes})
+    this.setState({fishes});
+}
+
     render() {
         return (
             <div className="catch-of-the-day">
@@ -11,7 +35,7 @@ class App extends React.Component {
                     <Header tagline="Fresh Seafood Market" />
                 </div>
                 <Order />
-                <Inventory/>
+                <Inventory addFish={this.addFish} />
             </div>
 
         )
