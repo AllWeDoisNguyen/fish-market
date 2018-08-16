fe24c2db4d55fcf841ea1afdba070bd19bece5a8
Christina Nguyen <cnguyen2@sapient.com>
2018-08-16 16:30:54 -0400

    create fish component and rendering it out

---
+++ src/components/Fish.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Fish extends React.Component {
+    render() {
+        return (
+            <li className="menu-fish">🍣</li>
+        )
+    }
+}
+
+export default Fish;
