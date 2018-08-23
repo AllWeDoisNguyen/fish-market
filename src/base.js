388c4932e0da1088bc3d3a1725dc5baadae0b812
Christina Nguyen <cnguyen2@sapient.com>
2018-08-23 16:43:31 -0400

    adding in firebase to persist data

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyAiBQ9UG-1PL6r0QoSf7_UU09QM0h-4ADg",
+    authDomain: "fish-market-catch-of-the-day.firebaseapp.com",
+    databaseURL: "https://fish-market-catch-of-the-day.firebaseio.com",
+});
+
+export default base;
