69294f16f752582ee3c78d30df532887134984a4
Christina Nguyen <cnguyen2@sapient.com>
2018-08-28 14:49:37 -0400

    add method to remove fish from app

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,7 @@ class App extends React.Component {
 
         this.addFish = this.addFish.bind(this);
         this.updateFish = this.updateFish.bind(this);
+        this.removeFish = this.removeFish.bind(this);
         this.loadSamples = this.loadSamples.bind(this);
         this.addToOrder = this.addToOrder.bind(this);
 
@@ -66,6 +67,12 @@ class App extends React.Component {
         const fishes = {...this.state.fishes};
         fishes[key] = updatedFish;
         this.setState({fishes});
+    }
+
+    removeFish(key) {
+        const fishes = {...this.state.fishes};
+        fishes[key] = null;
+        this.setState({ fishes });
     }
 
     loadSamples() {
